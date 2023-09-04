31f16eb28c77625fce5b55c351af20fa0ad5215c
PHGrespan <phgrespan@gmail.com>
2023-09-03 21:44:15 -0300

    fix: Correção da criação das tabelas

--- src/main/resources/db/create_tables.sql
+++ src/main/resources/db/create_tables.sql
@@ -1,75 +1,76 @@
+DROP DATABASE project_management;
 CREATE DATABASE project_management;
 USE project_management;
 
 CREATE TABLE user
 (
-    id_user      int          NOT NULL AUTO_INCREMENT,
-    name         varchar(255) NOT NULL,
-    email        varchar(255) NOT NULL,
-    pass         varchar(255) NOT NULL,
+    user_id      SERIAL,
+    name         VARCHAR(255) NOT NULL,
+    email        VARCHAR(255) NOT NULL,
+    pass         VARCHAR(255) NOT NULL,
 
-    creationDate DATETIME     NOT NULL,
+    creation_date DATETIME     NOT NULL,
 
-    PRIMARY KEY (id_user)
+    PRIMARY KEY (user_id)
 );
 
 CREATE TABLE workspace
 (
-    id_workspace int          NOT NULL AUTO_INCREMENT,
-    name         varchar(255) NOT NULL,
-    description  varchar(255) NOT NULL,
+    workspace_id SERIAL,
+    name         VARCHAR(255) NOT NULL,
+    description  VARCHAR(255) NOT NULL,
 
-    creationDate DATETIME     NOT NULL,
-    updateDate   DATETIME     NOT NULL,
+    creation_date DATETIME     NOT NULL,
+    update_date   DATETIME     NOT NULL,
 
-    PRIMARY KEY (id_workspace)
+    PRIMARY KEY (workspace_id)
 );
 
 CREATE TABLE project
 (
-    id_project         int          NOT NULL AUTO_INCREMENT,
-    name               varchar(255) NOT NULL,
-    description        varchar(255) NOT NULL,
+    project_id         SERIAL,
+    name               VARCHAR(255) NOT NULL,
+    description        VARCHAR(255) NOT NULL,
 
-    id_workspace       int          NOT NULL,
-    workspace_position int          NOT NULL,
+    workspace_id       BIGINT UNSIGNED,
+    workspace_position INT          NOT NULL,
 
-    PRIMARY KEY (id_project),
-    FOREIGN KEY (id_workspace) REFERENCES workspace (id_workspace)
+    PRIMARY KEY (project_id),
+    FOREIGN KEY (workspace_id) REFERENCES workspace (workspace_id)
 );
 
-CREATE TABLE list
+CREATE TABLE catalog
 (
-    id_list          int          NOT NULL AUTO_INCREMENT,
-    name             varchar(255) NOT NULL,
+    catalog_id       SERIAL,
+    name             VARCHAR(255) NOT NULL,
 
-    id_project       int          NOT NULL,
-    project_position int          NOT NULL,
+    project_id       BIGINT UNSIGNED,
+    project_position INT          NOT NULL,
 
-    PRIMARY KEY (id_list),
-    FOREIGN KEY (id_project) REFERENCES project (id_project)
+    PRIMARY KEY (catalog_id),
+    FOREIGN KEY (project_id) REFERENCES project (project_id)
 );
 
 CREATE TABLE card
 (
-    id_card       int          NOT NULL AUTO_INCREMENT,
-    name          varchar(255) NOT NULL,
-    description   varchar(255) NOT NULL,
+    card_id       SERIAL,
+    name          VARCHAR(255) NOT NULL,
+    description   VARCHAR(255) NOT NULL,
 
-    id_list       int          NOT NULL,
-    list_position int          NOT NULL,
+    catalog_id    BIGINT UNSIGNED,
+    list_position INT          NOT NULL,
 
-    PRIMARY KEY (id_card),
-    FOREIGN KEY (id_list) REFERENCES list (id_list)
+    PRIMARY KEY (card_id),
+    FOREIGN KEY (catalog_id) REFERENCES catalog (catalog_id)
 );
 
 CREATE TABLE user_workspace
 (
-    id_user      int     NOT NULL,
-    id_workspace int     NOT NULL,
+    user_id      BIGINT UNSIGNED,
+    workspace_id BIGINT UNSIGNED,
     owner        boolean NOT NULL,
 
-    PRIMARY KEY (id_user, id_workspace),
-    FOREIGN KEY (id_user) REFERENCES user (id_user),
-    FOREIGN KEY (id_workspace) REFERENCES workspace (id_workspace)
+    PRIMARY KEY (user_id, workspace_id),
+    FOREIGN KEY (user_id) REFERENCES user (user_id),
+    FOREIGN KEY (workspace_id) REFERENCES workspace (workspace_id)
 );
