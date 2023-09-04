9c0cada947a94f8e05153799250532cbfc4aa6d5
PHGrespan <phgrespan@gmail.com>
2023-09-03 23:31:59 -0300

    feat: Criptografia da senha do banco de dados

--- src/main/java/br/edu/unifaj/ProjectManagementApplication.java
+++ src/main/java/br/edu/unifaj/ProjectManagementApplication.java
@@ -1,9 +1,11 @@
 package br.edu.unifaj;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableEncryptableProperties
 public class ProjectManagementApplication {
 
 	public static void main(String[] args) {
