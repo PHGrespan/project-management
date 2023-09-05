b1b167bfeaec3e4e76443a478a42e4cbd183ef62
PHGrespan <phgrespan@gmail.com>
2023-09-05 01:45:09 -0300

    feat: remove User getAll

--- src/main/java/br/edu/unifaj/controller/UserController.java
+++ src/main/java/br/edu/unifaj/controller/UserController.java
@@ -1,35 +1,32 @@
 package br.edu.unifaj.controller;
 
-import br.edu.unifaj.domain.User;
-import br.edu.unifaj.dto.model.UserDto;
+import br.edu.unifaj.dto.UserDto;
+import br.edu.unifaj.entity.User;
 import br.edu.unifaj.service.UserService;
+import br.edu.unifaj.view.View;
+import com.fasterxml.jackson.annotation.JsonView;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
 @RestController
 @RequestMapping("/users")
 public class UserController {
 
     @Autowired
-    UserService service;
+    UserService userService;
 
-    @GetMapping
-    public ResponseEntity<List<User>> getAllUsers() {
-        return new ResponseEntity<>(service.findAll(), HttpStatus.OK);
-    }
-
+    @JsonView(View.User.class)
     @PostMapping
     public ResponseEntity<User> insertUser(@Valid @RequestBody UserDto dto) {
-        return new ResponseEntity<>(service.save(dto), HttpStatus.CREATED);
+        return new ResponseEntity<>(userService.save(dto), HttpStatus.CREATED);
     }
 
+    @JsonView(View.User.class)
     @PutMapping("/{id}")
     public ResponseEntity<User> updateUser(@PathVariable(value = "id") Long id, @Valid @RequestBody UserDto dto) throws Exception {
-        return new ResponseEntity<>(service.update(id, dto), HttpStatus.OK);
+        return new ResponseEntity<>(userService.update(id, dto), HttpStatus.OK);
     }
 }
--- src/main/java/br/edu/unifaj/service/UserService.java
+++ src/main/java/br/edu/unifaj/service/UserService.java
@@ -1,40 +1,35 @@
 package br.edu.unifaj.service;
 
-import br.edu.unifaj.domain.User;
-import br.edu.unifaj.dto.model.UserDto;
+import br.edu.unifaj.entity.User;
+import br.edu.unifaj.dto.UserDto;
 import br.edu.unifaj.mapper.UserMapper;
 import br.edu.unifaj.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
-import java.util.List;
 
 @Service
 public class UserService {
 
     @Autowired
-    UserRepository repository;
-
-    public List<User> findAll() {
-        return repository.findAll();
-    }
+    UserRepository userRepository;
 
     public User save(UserDto dto) {
         User newUser = UserMapper.INSTANCE.UserDtoToUser(dto);
 
         newUser.setCreationDate(LocalDateTime.now());
 
-        return repository.save(newUser);
+        return userRepository.save(newUser);
     }
 
     public User update(Long id, UserDto dto) throws Exception {
-        User oldUser = repository.findById(id).orElseThrow(() -> new Exception("User not found"));
+        User oldUser = userRepository.findById(id).orElseThrow(() -> new Exception("User not found"));
 
         User newUser = UserMapper.INSTANCE.UserDtoToUser(dto);
         newUser.setId(oldUser.getId());
         newUser.setCreationDate(oldUser.getCreationDate());
 
-        return repository.save(newUser);
+        return userRepository.save(newUser);
     }
 }
