baef09c4f728354c3c70785ad0fd720260f7dd59
PHGrespan <phgrespan@gmail.com>
2023-09-10 22:34:46 -0300

    feat: change response json (userWorkspaces to workspaces)

--- src/main/java/br/edu/unifaj/entity/User.java
+++ src/main/java/br/edu/unifaj/entity/User.java
@@ -2,6 +2,7 @@ package br.edu.unifaj.entity;
 
 import br.edu.unifaj.view.View;
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonView;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
@@ -49,6 +50,7 @@ public class User {
     private LocalDateTime creationDate;
 
     @JsonView(View.Workspace.class)
+    @JsonProperty("workspaces")
     @OneToMany(mappedBy = "user", cascade = CascadeType.REMOVE, orphanRemoval = true)
     private List<UserWorkspace> userWorkspaces = new ArrayList<>();
 
