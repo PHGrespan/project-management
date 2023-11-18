7793a25c62e78de10b95c3166d91291ddb20b503
PHGrespan <phgrespan@gmail.com>
2023-11-18 14:58:19 -0300

    feat: CrossOrigin

--- src/main/java/br/edu/unifaj/controller/rest/ProjectRestController.java
+++ src/main/java/br/edu/unifaj/controller/rest/ProjectRestController.java
@@ -13,6 +13,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:63343")
 public class ProjectRestController {
 
     @Autowired
